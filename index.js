55a77a31dffde940e525a94e280536e03a1cafeb
Smirnov Dmitry <sdo@mail.ru>
2018-04-19 09:30:50 +0700

    new keyboard

--- index.js
+++ index.js
@@ -86,7 +86,6 @@ var complite = 0;
 
 bot.start();
 
-
 bot.on(/\/start/, function (msg, match) {
     let replyMarkup = bot.keyboard([
         ['Курсы валют']
@@ -100,6 +99,7 @@ bot.on(/\/start/, function (msg, match) {
 });
 
 bot.on('Курсы валют', msg => {
+    bot.sendMessage(msg.from.id,'Запрос к биржам. Ожидайте...');
     request.get('https://api.exmo.com/v1/order_book/?pair=BTC_RUB&limit=1', (error, response, body) => {
         complite = 0;
         result = [];
@@ -148,77 +148,3 @@ bot.on('Курсы валют', msg => {
     });
 });
 
-bot.on('message', function onCallbackQuery(msg) {
-    date = new Date(1000 * msg.date);
-    console.log('from: id:', msg.from.id, ' name: ', msg.from.last_name + ' ' + msg.from.first_name, ' time: ', date);
-    if (whiteList.indexOf(msg.from.id) == -1) {
-        console.log(msg.from.id, ': access denied');
-        bot.sendMessage(msg.from.id, "Курсы с биржи предоставляются по подписке.");
-    } else {
-
-        if (msg.text == "Курсы валют") {
-            request.get('https://api.exmo.com/v1/order_book/?pair=BTC_RUB&limit=1', function (error, response, body) {
-                complite = 0;
-                result = [];
-
-                if (!error && response.statusCode == 200) {
-                    let course = parseFloat(JSON.parse(body).BTC_RUB.bid_top);
-                    result.push('RUB-BTC: ' + course.toFixed(2));
-                    urls.forEach(pair => {
-                        request.get(pair.url, function (error, response, body) {
-                            if (!error && response.statusCode == 200) {
-                                bxJSON = JSON.parse(body);
-
-                                try {
-                                    if (pair.birga == 'bittrex')
-                                        r = parseFloat(bxJSON.result.Bid);
-                                    else {
-                                        r = parseFloat(bxJSON.bids[0][0]);
-                                    }
-
-                                    result.push(pair.pair + ': ' + (r * course).toFixed(2));
-                                    complite++;
-                                    if (complite == urls.length) {
-                                        let r = '';
-                                        result.sort().forEach(item => {
-                                            r = r + item + '\n';
-                                        });
-                                        bot.sendMessage(msg.from.id, r);
-                                    }
-                                } catch (error) {
-                                    result.push(pair.pair + ': ОШИБКА!');
-                                    complite++;
-                                }
-
-
-                            } else {
-                                result.push(pair.pair + ': ОШИБКА!');
-                                complite++;
-                                bot.sendMessage(msg.from.id, 'Ошибка получения курса' + url.pair);
-                            }
-                        })
-
-                    })
-
-
-                } else {
-                    bot.sendMessage(msg.from.id, 'Ошибка получения курса BTC->RUB');
-                }
-            })
-        } else if (msg.text == "Валюты EXMO") {
-            request.get('https://api.exmo.com/v1/currency', function (error, response, body) {
-                res = JSON.parse(body);
-                r = '';
-                res.sort().forEach(item => {
-                    r = r + item + '\n';
-                });
-                bot.sendMessage(msg.from.id, r);
-            })
-
-        } else {
-            bot.sendMessage(msg.from.id, 'Я не понимаю это сообщение');
-        }
-    }
-
-
-});
